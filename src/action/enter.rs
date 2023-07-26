a88e0490269f03ef9006319c78ac734cf09acf53
Teajey <21069848+Teajey@users.noreply.github.com>
2023-07-26 19:35:23 +0100

    fix: display local time on punch in

--- src/action/enter.rs
+++ src/action/enter.rs
@@ -5,7 +5,10 @@ use crate::{error::Result, record::Record};
 pub fn run(record: &mut Record<Utc>) -> Result<()> {
     let clock_in_time = record.clock_in()?;
 
-    println!("Clocked in at {}", clock_in_time.format("%c"));
+    println!(
+        "Clocked in at {}",
+        clock_in_time.with_timezone(&chrono::Local).format("%c")
+    );
 
     Ok(())
 }

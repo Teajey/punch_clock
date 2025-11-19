0b2a90031c2aab1e61b4d22982b632eae23d6eb0
Teajey <21069848+Teajey@users.noreply.github.com>
2025-11-19 18:06:19 +0900

    feat: set versions with environment variables Instead of writing to files

--- build.rs
+++ build.rs
@@ -7,28 +7,13 @@ use std::process::Command;
 fn main() {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set");
 
-    let git_version_path = Path::new(&manifest_dir).join("src/git-version.txt");
-
     let status_output = Command::new("git")
         .args(["status", "--porcelain"])
         .output()
         .expect("Failed to execute git status command");
     let in_git_repo = status_output.status.success();
-    let git_vers_exists = git_version_path.exists();
 
-    assert!(
-        in_git_repo || git_vers_exists,
-        "Panicking because there's no .git and {} otherwise cannot be determined",
-        git_version_path.display()
-    );
-
-    if git_vers_exists && !in_git_repo {
-        println!(
-            "cargo:warning=Because a .git could not be found, the existing {} will be used",
-            git_version_path.display()
-        );
-        return;
-    }
+    assert!(in_git_repo, "Panicking because there's no .git",);
 
     let git2 = vergen_git2::Git2Builder::default()
         .sha(false)
@@ -62,15 +47,8 @@ fn main() {
         println!("cargo:warning=Repository is dirty, adding * to commit hash");
     }
 
-    let mut file =
-        fs::File::create(&git_version_path).expect("Failed to create git commit hash file");
-    file.write_all(git_version.as_bytes())
-        .expect("Failed to write git commit hash to file");
-
-    println!(
-        "cargo:warning=Git commit hash written to {}",
-        git_version_path.display()
-    );
+    println!("cargo:rustc-env=PUNCH_CLOCK_GIT_REVISION={git_version}");
+    println!("cargo:warning=Git commit revision exported to PUNCH_CLOCK_GIT_REVISION",);
 
     let manifest_path = Path::new(&manifest_dir).join("Cargo.toml");
     let manifest = std::fs::read_to_string(&manifest_path).unwrap();
@@ -78,14 +56,8 @@ fn main() {
     let crate_version = manifest["package"]["version"]
         .as_str()
         .expect("missing crate version");
-    let long_version_path = Path::new(&manifest_dir).join("src/long-version.txt");
 
-    let mut file =
-        fs::File::create(&long_version_path).expect("Failed to create long version file");
-    write!(file, "{crate_version} {git_version}").expect("Failed to write long version to file");
-
-    println!(
-        "cargo:warning=Long version written to {}",
-        long_version_path.display()
-    );
+    let long_version = format!("{crate_version} {git_version}");
+    println!("cargo:rustc-env=PUNCH_CLOCK_LONG_VERSION={long_version}");
+    println!("cargo:warning=Long version {long_version:?} exported to PUNCH_CLOCK_GIT_REVISION",);
 }
